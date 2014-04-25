52744dc0bca9e1bcd34753245e9e7c4807623c43
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2014-04-25 11:56:04 -0300

    change the license and add default recipe description

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,15 @@
 name             'dokuwiki'
 maintainer       'Pablo Gutierrez del Castillo'
 maintainer_email 'pablogutierrezdelc@gmail.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures dokuwiki'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-supports         'ubuntu'
+supports 'ubuntu'
 
-depends          'apt'
-depends          'ark'
-depends          'nginx'
+depends 'apt'
+depends 'ark'
+depends 'nginx'
+
+recipe 'default', 'Installs and configures nginx, php-fpm and dokuwiki'
