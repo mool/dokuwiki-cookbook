ae02c95f612f00f0da855e0da9741e4b43e819b6
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2014-04-25 12:11:43 -0300

    version 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pablogutierrezdelc@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures dokuwiki'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 supports 'ubuntu'
 
