8a07855a1c17f0f19f44de8db33929ffb7166d46
Peter Green <peter.green@aztek-native.com>
2015-03-05 09:37:36 +0000

    Added attributes and modified config template to configure LDAP.

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,5 +9,21 @@ default['dokuwiki']['admin']['pass'] = '$1$1e1a49b8$40q83WGBdZD06BeSRRRRU/'
 default['dokuwiki']['admin']['name'] = 'Administrator'
 default['dokuwiki']['admin']['mail'] = 'admin@dokuwiki.org'
 
+default['dokuwiki']['authtype'] = "authplain"
+default['dokuwiki']['useacl'] = 1
+default['dokuwiki']['plugin']['authldap']['server'] = "ldap://ldap.example.com:389"
+default['dokuwiki']['plugin']['authldap']['usertree'] = "ou=People,dc=example,dc=com"
+default['dokuwiki']['plugin']['authldap']['grouptree'] = "ou=Group,dc=example,dc=com"
+default['dokuwiki']['plugin']['authldap']['userfilter'] = "(&(uid=%{user})(objectClass=posixAccount))"
+default['dokuwiki']['plugin']['authldap']['groupfilter'] = "(&(objectClass=posixGroup)(memberUID=%{user}))"
+default['dokuwiki']['plugin']['authldap']['version'] = 3
+default['dokuwiki']['plugin']['authldap']['starttls'] = 1
+default['dokuwiki']['plugin']['authldap']['referrals'] = 0
+default['dokuwiki']['plugin']['authldap']['binddn'] = "cn=admin, dc=my, dc=home"
+default['dokuwiki']['plugin']['authldap']['bindpw'] = "secret"
+default['dokuwiki']['plugin']['authldap']['userscope'] = "sub"
+default['dokuwiki']['plugin']['authldap']['groupscope'] = "sub"
+default['dokuwiki']['plugin']['authldap']['debug'] = 0
+
 # Nginx config
 default['nginx']['default_site_enabled'] = false
